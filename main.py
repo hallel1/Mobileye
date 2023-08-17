31bd6345895af26116ccadc499637a5c96a064c3
Hallel <halleli100@gmail.com>
2023-08-17 19:54:41 +0300

    set conf spark

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from pyspark.sql import SparkSession
+from pyspark import SparkConf, SparkContext
 
 import consts
 
+import os
+
+os.environ["PYSPARK_PYTHON"] = "C:\\Python310\\python.exe"
+os.environ["PYSPARK_DRIVER_PYTHON"] = "C:\\Python310\\python.exe"
+os.environ["SPARK_HOME"] = "C:\\spark\\spark-3.4.1-bin-hadoop3"
+
 
 def main():
-    spark = SparkSession.builder.appName(consts.SPARK_APP_NAME).getOrCreate()
+    conf = SparkConf().setAppName(consts.SPARK_APP_NAME)
+    spark = SparkSession.builder.config(conf=conf).getOrCreate()
 
     spark.stop()
 
