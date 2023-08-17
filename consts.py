e1c7ce873828bc30591756d5851307bffb4afa0e
Hallel <halleli100@gmail.com>
2023-08-17 17:44:16 +0300

    create schemas

---
+++ consts.py
@@ -0,0 +1,27 @@
+CREATE_DETECTION_DATA_TABLE = '''
+CREATE TABLE IF NOT EXISTS detection_data (
+    detection_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    vehicle_id TEXT NOT NULL,
+    detection_time TEXT NOT NULL,
+    object_type TEXT NOT NULL,
+    object_value INTEGER NOT NULL
+)
+'''
+
+INSERT_DETECTION_DATA = '''
+INSERT INTO detection_data (vehicle_id, detection_time, object_type, object_value)
+VALUES (?, ?, ?, ?)
+'''
+
+CREATE_VEHICLE_STATUS_TABLE = '''
+CREATE TABLE IF NOT EXISTS vehicle_status (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    vehicle_id TEXT NOT NULL,
+    report_time TEXT NOT NULL,
+    status TEXT NOT NULL
+);
+'''
+
+INSERT_VEHICLE_STATUS = '''
+INSERT INTO vehicle_status (vehicle_id, report_time, status) VALUES (?, ?, ?);
+'''
