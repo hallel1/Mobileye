5db9be5439fa364e20e2185cf6a062d9627e182a
Hallel <halleli100@gmail.com>
2023-08-19 01:16:41 +0300

    saperate function

--- main.py
+++ main.py
@@ -15,27 +15,30 @@ def main():
     set_spark_config()
     conf = SparkConf().setAppName(consts.SPARK_APP_NAME)
     spark = SparkSession.builder.config(conf=conf).getOrCreate()
-
-    watcher_files(spark)
+    
+    file_queue, observer = watcher_setting(spark)
+    watcher_files(spark, file_queue, observer)
 
     spark.stop()
 
 
-def watcher_files(spark):
+def watcher_setting(spark):
     file_queue = queue.Queue()
-    observer = Observer()
     event_handler = HandlerWatcher(file_queue)
     event_handler.set_spark(spark)
+
+    observer = Observer()
     observer.schedule(event_handler, consts.DIRECTORY_TO_WATCH, recursive=True)
+    return file_queue, observer
+
+
+def watcher_files(spark, file_queue, observer):
     observer.start()
     try:
         while True:
             if not file_queue.empty():
                 file_path = file_queue.get()
-                data = read_file(file_path)
-                raw_df = create_dataframe_from_json(spark, data)
-                df = identifier_file(file_path, raw_df)
-                df.printSchema()
+                run_new_file_process(spark, file_path)
             else:
                 time.sleep(1)
     except KeyboardInterrupt:
@@ -44,6 +47,13 @@ def watcher_files(spark):
     except Exception as e:
         print(e)
     observer.join()
+
+
+def run_new_file_process(spark, file_path):
+    data = read_file(file_path)
+    raw_df = create_dataframe_from_json(spark, data)
+    df = identifier_file(file_path, raw_df)
+    df.printSchema()
 
 
 if __name__ == "__main__":
