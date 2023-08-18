06c9fef53b32cad2310ba5d6cb600ca961df14dc
Hallel <halleli100@gmail.com>
2023-08-18 18:22:49 +0300

    ctrl alt l

--- main.py
+++ main.py
@@ -10,17 +10,21 @@ def main():
     spark = SparkSession.builder.config(conf=conf).getOrCreate()
     df = spark.read.json(spark.sparkContext.parallelize([consts.data]))
 
+
+def read_objects_detection_events(df):
     df_exploded = df.select(F.explode("objects_detection_events").alias("events"))
 
-    df_detections = (df_exploded
-    .select("events.vehicle_id", "events.detection_time",
-            F.explode("events.detections").alias("detection"))
-    .select(
-        "vehicle_id",
-        "detection_time",
-        "detection.object_type",
-        "detection.object_value"
-    ))
+    df_detections = df_exploded \
+        .select("events.vehicle_id", "events.detection_time",
+                F.explode("events.detections").alias("detection")) \
+        .select("vehicle_id",
+                "detection_time",
+                "detection.object_type",
+                "detection.object_value")
+
+    return df_detections
+
+
 def read_vehicle_status(df):
     df_exploded = df.select(F.explode("vehicle_status").alias("vehicle_status"))
 
