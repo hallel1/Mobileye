a6f60dd9f6def44a4ed50c36df2ff8911241ff16
Hallel <halleli100@gmail.com>
2023-08-18 18:20:26 +0300

    add read_vehicle_status function

--- main.py
+++ main.py
@@ -27,8 +27,15 @@ def main():
         "detection.object_type",
         "detection.object_value"
     ))
+def read_vehicle_status(df):
+    df_exploded = df.select(F.explode("vehicle_status").alias("vehicle_status"))
 
-    spark.stop()
+    df_detections = df_exploded \
+        .select("vehicle_status.vehicle_id",
+                "vehicle_status.report_time",
+                "vehicle_status.status")
+
+    return df_detections
 
 
 if __name__ == "__main__":
